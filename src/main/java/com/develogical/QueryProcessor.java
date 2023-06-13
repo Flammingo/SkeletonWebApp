d1a98069cfb6f8806122c00fd4f85c9b5f9e8443
Flammingo <45210199+Flammingo@users.noreply.github.com>
2023-06-13 14:43:40 +0000

    Updated Configuration

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
           + "writer in the English language and the world's pre-eminent dramatist.";
     }
 
-    if (query.toLowerCase().contains("your name")) {
+    if (query.toLowerCase().contains("dummyduck")) {
       return "RobTest";
     }
 

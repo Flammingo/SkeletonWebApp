34fa649b9a09130c4a39c1a373a55e62f494ae4b
Flammingo <45210199+Flammingo@users.noreply.github.com>
2023-06-13 14:54:30 +0000

    Changes 1234

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -12,7 +12,7 @@ public class QueryProcessorTest {
 
   @Test
   public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
-    assertThat(queryProcessor.process("playwright"), is(""));
+    assertThat(queryProcessor.process("test"), is(""));
   }
 
   @Test
